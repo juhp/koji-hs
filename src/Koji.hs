f687ae5bc996e3b9b482f3fa90c95f6e5ef1ddce
Jens Petersen <petersen@redhat.com>
2020-01-25 01:26:31 +0800

    switch to haxr with nil patch

--- src/Koji.hs
+++ src/Koji.hs
@@ -1,95 +1,120 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Koji
-       ( kojiCall,
-         hello,
-         listTags,
-         listTagsBuild,
-         listTagsPackage,
-         getPackageID,
-         getTagID,
-         listPackagesSimple,
-         checkTagPackage,
-         getLatestBuild,
-         getTaskInfo,
-         TaskState(..),
-         getBuild
-       ) where
+       (
+         koji
+       , hello
+       , getPackageID
+       , getTagID
+       , listPackagesSimple
+       , listTags
+       , listTagsBuild
+       , listTagsPackage
+       , checkTagPackage
+       , getLatestBuild
+       , getTaskInfo
+       , getBuild
+       , listTasks
+       )
+where
 
-import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as B
-import Data.Text (Text)
-import Optics.Core
-import Data.Aeson.Optics
-import System.Process.Typed
+--import Control.Monad.Fail (MonadFail)
+--import qualified Data.ByteString.Lazy.Char8 as B
+import Network.XmlRpc.Client
+import Network.XmlRpc.Internals
+--import Network.HTTP.Simple
+--import Network.HTTP.Client.Conduit
 
-kojiCall :: String -> [String] -> IO Value
-kojiCall c args = do
-  (out,err) <- readProcess_ (proc "koji" ("call":"--json-output":c:args))
-  case decode out of
-    Nothing -> error (B.unpack err) -- $ "Failure for: koji call " ++ unwords (c:args)
-    Just v -> return v
+hub :: String
+hub = "https://koji.fedoraproject.org/kojihub"
+---hub = "https://brewhub.engineering.redhat.com/brewhub"
 
-listTags :: IO [Text]
-listTags = do
-  res <- kojiCall "listTags" []
-  return $ res ^.. values % key "name" % _String
+koji :: Remote a => String -> a
+koji c = remote hub c
+--koji c = remoteWithHeaders hub c [("User-Agent", "koji/1")]
 
-listTagsBuild :: String -> IO [Text]
-listTagsBuild bld = do
-  res <- kojiCall "listTags" [bld]
-  return $ res ^.. values % key "name" % _String
+-- xmlrpc :: String -> String -> [Value] -> IO Value
+-- xmlrpc url m args = do
+--   initreq <- parseRequest url
+--   let reqbody = renderCall (MethodCall m args)
+--       request = setRequestBody (RequestBodyLBS reqbody) $ setRequestMethod "POST" initreq
+--   resp <- httpLBS request
+--   let respbody = getResponseBody resp
+--   handleError fail $ (parseResponse (B.unpack respbody) >>= handleResponse)
+--   where
+--     -- | Gets the return value from a method response.
+--     --   Throws an exception if the response was a fault.
+--     handleResponse :: MonadFail m => MethodResponse -> m Value
+--     handleResponse (Return v)       = return v
+--     handleResponse (Fault code str) = error ("Error " ++ show code ++ ": " ++ str)
 
-listTagsPackage :: String -> IO [Text]
-listTagsPackage pkg = do
-  res <- kojiCall "listTags" ["None", pkg]
-  return $ res ^.. values % key "name" % _String
+hello :: IO String
+hello = koji "hello"
 
-hello :: IO (Maybe Text)
-hello = do
-  res <- kojiCall "hello" []
-  return $ res ^? _String
+getPackageID :: String -> IO Int
+getPackageID pkg =
+  koji "getPackageID" pkg
 
-getPackageID :: String -> IO (Maybe Integer)
-getPackageID pkg = do
-  res <- kojiCall "getPackageID" [pkg]
-  return $ res ^? _Integer
+getTagID :: String -> IO Int
+getTagID tag =
+  koji "getTagID" tag
 
-getTagID :: String -> IO (Maybe Integer)
-getTagID tag = do
-  res <- kojiCall "getTagID" [tag]
-  return $ res ^? _Integer
+type Struct = [(String,Value)]
 
-listPackagesSimple :: String -> IO [Text]
+listPackagesSimple :: String -> IO [Struct]
 listPackagesSimple prefix = do
-  res <- kojiCall "listPackagesSimple" [prefix]
-  return $ res ^.. values % key "package_name" % _String
+  koji "listPackagesSimple" prefix
+
+listTags :: IO Value
+listTags =
+  koji "listTags"
+
+listTagsBuild :: Int -> IO Value
+listTagsBuild buildid =
+  koji "listTags" buildid ()
+
+listTagsPackage :: Int -> IO Value
+listTagsPackage pkgid =
+  koji "listTags" () pkgid
 
 checkTagPackage :: Int -> Int -> IO Bool
-checkTagPackage tagid pkgid = do
-  res <- kojiCall "checkTagPackage" [show tagid,show pkgid]
-  return (res ^? _Bool == Just True)
+checkTagPackage tagid pkgid =
+  koji "checkTagPackage" tagid pkgid
 
-getLatestBuild :: String -> String -> IO (Maybe Text)
-getLatestBuild tag pkg = do
-  res <- kojiCall "getLatestBuilds" [tag, "None", pkg]
-  return $ res ^? key "nvr" % _String
+getLatestBuild :: String -> String -> IO Value
+getLatestBuild tag pkg =
+  koji "getLatestBuilds" tag () pkg
 
-data TaskState = FREE | OPEN | CLOSED | CANCELED | ASSIGNED | FAILED
-  deriving (Eq, Enum)
+-- data TaskState = FREE | OPEN | CLOSED | CANCELED | ASSIGNED | FAILED
+--   deriving (Eq, Enum)
 
 --data TaskInfo = TaskInfo String TaskState
 
-getTaskInfo :: Int -> IO Value --(Maybe TaskState)
-getTaskInfo taskid = do
-  {-res <--} kojiCall "getTaskInfo" [show taskid]
+getTaskInfo :: Int -> IO Struct
+getTaskInfo taskid =
+  koji "getTaskInfo" taskid
+  -- res <- kojiCall "getTaskInfo" [show taskid]
   -- let state = res ^? key "state" % _Integer <&> (toEnum . fromInteger)
   --     arch = res ^? key "arch" % _String
   -- return $ TaskInfo arch state
 
-getBuild :: Int -> IO Value
-getBuild buildid = do
-  {-res <--} kojiCall "getBuild" [show buildid]
---  return $ res ^? key "state" % _Integer <&> (toEnum . fromInteger)
+getBuild :: Int -> IO Struct
+getBuild buildid =
+  koji "getBuild" buildid
 
+data TaskState = FREE | OPEN | CLOSED | CANCELED | ASSIGNED | FAILED
+  deriving (Eq, Enum)
+
+openstates :: Value
+openstates = ValueArray $ map (ValueInt . fromEnum) [FREE, OPEN, ASSIGNED]
+
+stateToValue :: TaskState -> Value
+stateToValue = ValueInt . fromEnum
+
+readState :: Value -> TaskState
+readState (ValueInt i) | i `elem` (map fromEnum (enumFrom FREE)) = toEnum i
+readState _ = error "invalid task state"
+
+listTasks :: Struct -> Struct -> IO [Struct]
+listTasks opts qopts = koji "listTasks" opts qopts
 
