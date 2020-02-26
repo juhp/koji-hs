6e824c68265902a9619ee393ca10881bcb3dc678
Jens Petersen <petersen@redhat.com>
2020-02-27 01:21:49 +0800

    add getUserID, ID class, export TaskState, TaskID, Value, Struct
    sufficient now for koji-progress

--- src/Koji.hs
+++ src/Koji.hs
@@ -6,25 +6,47 @@ module Koji
        , hello
        , getPackageID
        , getTagID
+       , getUserID
+       , getLatestBuild
+       , getTaskInfo
+       , getTaskChildren
+       , getTaskState
+       , getBuildById
+       , getBuildByNVR
        , listPackagesSimple
        , listTags
        , listTagsBuild
        , listTagsPackage
+       , listTaskIDs
        , checkTagPackage
-       , getLatestBuild
-       , getTaskInfo
-       , getBuildById
-       , getBuildByNVR
-       , listTasks
+       , TaskState(..)
+       , openstates
+       , stateToValue
+       , readState
+       , TaskID(..)
+       , Value(..)
+       , displayID
+       , getID
+       , readID
+       , readID'
+       , Struct
+       , lookupStruct
+--       , readStructString
+--       , readStructArray
+--       , readMethodParams
+       , maybeVal
+       , getValue
        )
 where
 
+import Data.Maybe
 --import Control.Monad.Fail (MonadFail)
 --import qualified Data.ByteString.Lazy.Char8 as B
 import Network.XmlRpc.Client
 import Network.XmlRpc.Internals
 --import Network.HTTP.Simple
 --import Network.HTTP.Client.Conduit
+import Control.Monad.Except (runExceptT)
 
 hub :: String
 hub = "https://koji.fedoraproject.org/kojihub"
@@ -52,15 +74,70 @@ koji = remote hub
 hello :: IO String
 hello = koji "hello"
 
+class ID a where
+  maybeID :: Maybe Value -> Maybe a
+  getID :: a -> Int
+
+displayID :: ID a => a -> String
+displayID = show . getID
+
+readID :: ID a => Struct -> Maybe a
+readID = maybeID . lookup "id"
+
+readID' :: Struct -> Maybe Int
+readID' st =
+  case lookup "id" st of
+    Just (ValueInt i) -> Just i
+    _ -> Nothing
+
+newtype TaskID = TaskId Int
+  deriving Show
+
+instance ID TaskID where
+  getID (TaskId i) = i
+  maybeID (Just (ValueInt i)) = Just (TaskId i)
+  maybeID _ = Nothing
+
+newtype TagID = TagId Int
+  deriving Show
+
+instance ID TagID where
+  getID (TagId i) = i
+  maybeID (Just (ValueInt i)) = Just (TagId i)
+  maybeID _ = Nothing
+
+newtype UserID = UserId Int
+  deriving Show
+
+instance ID UserID where
+  getID (UserId i) = i
+  maybeID (Just (ValueInt i)) = Just (UserId i)
+  maybeID _ = Nothing
+
+newtype BuildID = BuildId Int
+
+instance ID BuildID where
+  getID (BuildId i) = i
+  maybeID (Just (ValueInt i)) = Just (BuildId i)
+  maybeID _ = Nothing
+
+newtype PackageID = PackageId Int
+
+instance ID PackageID where
+  getID (PackageId i) = i
+  maybeID (Just (ValueInt i)) = Just (PackageId i)
+  maybeID _ = Nothing
+
 getPackageID :: String -- ^ package
-             -> IO Int -- ^ pkgid
-getPackageID =
-  koji "getPackageID"
+             -> IO PackageID
+getPackageID pkg =
+  PackageId <$> koji "getPackageID" pkg
+
 
 getTagID :: String -- ^ tag
-         -> IO Int -- ^ tagid
-getTagID =
-  koji "getTagID"
+         -> IO TagID
+getTagID tag =
+  TagId <$> koji "getTagID" tag
 
 type Struct = [(String,Value)]
 
@@ -73,21 +150,21 @@ listTags :: IO Value
 listTags =
   koji "listTags"
 
-listTagsBuild :: Int -- ^ buildid
+listTagsBuild :: BuildID -- ^ buildid
               -> IO Value
-listTagsBuild buildid =
-  koji "listTags" buildid ()
+listTagsBuild bid =
+  koji "listTags" (getID bid) ()
 
-listTagsPackage :: Int -- ^ pkgid
+listTagsPackage :: PackageID
                 -> IO Value
-listTagsPackage =
-  koji "listTags" ()
+listTagsPackage pid =
+  koji "listTags" () (getID pid)
 
-checkTagPackage :: Int -- ^ tagid
-                -> Int -- ^ pkgid
+checkTagPackage :: TagID
+                -> PackageID
                 -> IO Bool
-checkTagPackage =
-  koji "checkTagPackage"
+checkTagPackage tid pid =
+  koji "checkTagPackage" (getID tid) (getID pid)
 
 getLatestBuild :: String -- ^ tag
                -> String -- ^ pkg
@@ -95,24 +172,26 @@ getLatestBuild :: String -- ^ tag
 getLatestBuild tag =
   koji "getLatestBuilds" tag ()
 
--- data TaskState = FREE | OPEN | CLOSED | CANCELED | ASSIGNED | FAILED
---   deriving (Eq, Enum)
-
 --data TaskInfo = TaskInfo String TaskState
 
-getTaskInfo :: Int -- ^ taskid
+getTaskInfo :: TaskID
+            -> Bool -- ^ include request details
             -> IO Struct
-getTaskInfo =
-  koji "getTaskInfo"
+getTaskInfo tid =
+  koji "getTaskInfo" (getID tid)
   -- res <- kojiCall "getTaskInfo" [show taskid]
   -- let state = res ^? key "state" % _Integer <&> (toEnum . fromInteger)
   --     arch = res ^? key "arch" % _String
   -- return $ TaskInfo arch state
 
-getBuildById :: Int -- ^ buildid
-         -> IO (Maybe Struct)
+getTaskChildren :: TaskID -> Bool -> IO [Struct]
+getTaskChildren tid =
+  koji "getTaskChildren" (getID tid)
+
+getBuildById :: BuildID
+             -> IO (Maybe Struct)
 getBuildById bid =
-  maybeStruct <$> koji "getBuild" bid
+  maybeStruct <$> koji "getBuild" (getID bid)
 
 maybeStruct :: Value -> Maybe Struct
 maybeStruct (ValueStruct st) = Just st
@@ -123,20 +202,64 @@ getBuildByNVR :: String -- ^ NVR
 getBuildByNVR nvr =
   maybeStruct <$> koji "getBuild" nvr
 
--- data TaskState = FREE | OPEN | CLOSED | CANCELED | ASSIGNED | FAILED
---   deriving (Eq, Enum)
+data TaskState = FREE | OPEN | CLOSED | CANCELED | ASSIGNED | FAILED
+  deriving (Eq, Enum, Show)
 
--- openstates :: Value
--- openstates = ValueArray $ map (ValueInt . fromEnum) [FREE, OPEN, ASSIGNED]
+openstates :: Value
+openstates = ValueArray $ map (ValueInt . fromEnum) [FREE, OPEN, ASSIGNED]
 
--- stateToValue :: TaskState -> Value
--- stateToValue = ValueInt . fromEnum
+stateToValue :: TaskState -> Value
+stateToValue = ValueInt . fromEnum
 
--- readState :: Value -> TaskState
--- readState (ValueInt i) | i `elem` map fromEnum (enumFrom FREE) = toEnum i
--- readState _ = error "invalid task state"
+readState :: Value -> TaskState
+readState (ValueInt i) | i `elem` map fromEnum (enumFrom FREE) = toEnum i
+readState _ = error "invalid task state"
 
 listTasks :: Struct -- ^ opts
           -> Struct -- ^ qopts
           -> IO [Struct]
 listTasks = koji "listTasks"
+
+listTaskIDs :: Struct -- ^ opts
+          -> Struct -- ^ qopts
+          -> IO [TaskID]
+listTaskIDs opts qopts =
+  mapMaybe readID <$> listTasks opts qopts
+
+getUserID :: String -> IO (Maybe UserID)
+getUserID name = do
+  res <- maybeStruct <$> koji "getUser" name
+  return $ readID =<< res
+
+getTaskState :: Struct -> Maybe TaskState
+getTaskState st =
+  either error readState <$> runExceptT (getField "state" st)
+--  readState <$> lookup "state" st
+
+-- readStructString :: String -> Struct -> String
+-- readStructString key str =
+--   case lookup key str of
+--     Just (ValueString s) -> s
+--     _ -> error $ "No String for " ++ key
+
+-- readStructArray :: String -> Struct -> Maybe [Value]
+-- readStructArray key struct =
+--   either error id <$> runExceptT (getField key struct)
+-- --    Just (ValueArray s) -> s
+-- --    _ -> error $ "No Array for " ++ key
+
+-- readMethodParams :: String -> Maybe [Value]
+-- readMethodParams s =
+--   either error params <$> runExceptT (parseCall s)
+--   where
+--     params (MethodCall _m str) = str
+
+lookupStruct :: XmlRpcType a => String -> Struct -> Maybe a
+lookupStruct key struct =
+  either error id <$> runExceptT (getField key struct)
+
+maybeVal :: String -> Maybe a -> a
+maybeVal err = fromMaybe (error err)
+
+getValue :: XmlRpcType a => Value -> Maybe a
+getValue = fmap (either error id) . runExceptT . fromValue
